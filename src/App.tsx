a32904b8f73179c91baeffe3ef4461b702cd9920
bona <bona373737@gmail.com>
2023-03-09 13:53:27 +0900

    lint text commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  console.log("아하하")
+  var nana = "나나";
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
